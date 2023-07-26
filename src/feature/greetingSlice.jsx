65a9abc7416eb065888bab4c00eaa6b26e4cae27
david30-maker <davidokpah396@gmail.com>
2023-07-26 21:32:53 +0100

    requested change

--- src/feature/greetingSlice.jsx
+++ src/feature/greetingSlice.jsx
@@ -6,7 +6,7 @@ export const fetchGreeting = createAsyncThunk(
   async () => {
     try {
       const response = await axios.get(
-        'http://127.0.0.1:3000//api/random_greeting',
+        'http://127.0.0.1:3000/api/random_greeting',
       );
       return response.data;
     } catch (error) {
